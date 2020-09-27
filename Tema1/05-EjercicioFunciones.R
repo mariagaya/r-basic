e302ca9954e87c7fd91799b6f9c0908e1746ff93
mariagaya <mariagayafiol@gmail.com>
2020-09-27 12:15:48 +0200

    Ejercicio Funciones

---
+++ Tema1/05-EjercicioFunciones.R
@@ -0,0 +1,14 @@
+opBasic = function(x, y){
+  print('Suma')
+  print(x+y)
+  print('Resta')
+  print(paste(sprintf('%i - %i =',x,y),x-y))
+  print('Multiplicación')
+  print(x*y)
+  print('División')
+  print(x%/%y)
+  print('Resto')
+  print(x%%y)
+}
+
+opBasic(6,4)
