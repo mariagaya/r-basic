3e53596cd26a5a40430b24d2fcf1adc9b20a5564
mariagaya <mariagayafiol@gmail.com>
2020-09-27 14:54:16 +0200

    Más ejercicios

---
+++ Tema1/05-Variables.R
@@ -0,0 +1,24 @@
+x = 4
+
+x
+
+func = function(y){y^2}
+func(x)
+
+func1 <- function(t){
+  t^3 + t*5
+}
+
+func1(7)
+
+#Listar todos los elementos
+ls()
+
+#Remove a variable
+rm(x)
+
+ls()
+
+rm(list = ls())
+
+ls()
